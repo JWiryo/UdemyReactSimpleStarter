4624f8f1488689b6500517c1f72a584ed4910b8e
James Wiryo <jamesgiggs95@gmail.com>
2017-08-15 02:19:30 +0800

    Initialised Video List Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyC8fYgZ0FsXubMXKfbk-ZRqm4N8-hqm-_8";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    YTSearch({ key: API_KEY, term: "cats" }, videos => {
+      this.setState({ videos });
+    });
+
+    this.state = { videos: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on the page DOM
+ReactDOM.render(<App />, document.querySelector(".container"));
